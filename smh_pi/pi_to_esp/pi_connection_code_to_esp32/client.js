a2d16e2d681fcfcd980c27491e651a723e3f5519
ThePeterBrons <peterbrons2004@gmail.com>
2024-10-14 15:50:38 +0200

    testing still
    no notable notes to add

--- smh_pi/pi_to_esp/pi_connection_code_to_esp32/client.js
+++ smh_pi/pi_to_esp/pi_connection_code_to_esp32/client.js
@@ -1,19 +1,17 @@
-const express = require("express");
-const app = express();
-const port = 8000;
+// const express = require("express");
+// const app = express();
+// const port = 8000;
 
-app.use(express.static("static"));
+// app.use(express.static("static"));
 
-app.get("/", (req, res) => {
-	res.send("Test");
-});
-app.post("/data/{experminet_id}/humidty");
+// app.get("/", (req, res) => {
+// 	res.send("Test");
+// });
+// app.post("/data/{experminet_id}/humidty");
 
-app.listen(port, () => {
-	console.log(`Running on http://localhost:${port}`);
-});
-
-
+// app.listen(port, () => {
+// 	console.log(`Running on http://localhost:${port}`);
+// });
 
 
 
@@ -34,26 +32,28 @@ app.listen(port, () => {
 
 
 
-// const WebSocket = require('ws');
 
-// // Replace <ESP32_IP> with the IP address of your ESP32
-// const ws = new WebSocket('ws://<ESP32_IP>/ws');
 
-// ws.on('open', function open() {
-//   console.log('Connected to ESP32 WebSocket server');
+const WebSocket = require('ws');
+
+// Replace <ESP32_IP> with the IP address of your ESP32
+const ws = new WebSocket('ws://<ESP32_IP>/ws');
+
+ws.on('open', function open() {
+  console.log('Connected to ESP32 WebSocket server');
   
-//   // Send data to the ESP32
-//   ws.send('Hello ESP32, this is Raspberry Pi!');
-// });
+  // Send data to the ESP32
+  ws.send('Hello ESP32, this is Raspberry Pi!');
+});
 
-// ws.on('message', function incoming(data) {
-//   console.log(`Received message from ESP32: ${data}`);
-// });
+ws.on('message', function incoming(data) {
+  console.log(`Received message from ESP32: ${data}`);
+});
 
-// ws.on('close', function close() {
-//   console.log('Disconnected from WebSocket server');
-// });
+ws.on('close', function close() {
+  console.log('Disconnected from WebSocket server');
+});
 
-// ws.on('error', function error(err) {
-//   console.error('WebSocket error: ', err);
-// });
+ws.on('error', function error(err) {
+  console.error('WebSocket error: ', err);
+});
