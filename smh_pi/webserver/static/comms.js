259d715b4225d67e5dcec7fcd2f3d2bd8cee640a
Benedictus Leo <leoaryasatya@gmail.com>
2024-11-26 16:43:18 +0100

    Added accel graph reading

--- smh_pi/webserver/static/comms.js
+++ smh_pi/webserver/static/comms.js
@@ -16,7 +16,7 @@ socket.on("temperature", (t, sensorId) => {
 
 	// Add point to the temperature series and limit to 1000 points
 	if (chartTH.series[sensorId - 1].data.length > 100) {
-		chartTH.series[sensorId - 1].addPoint([x, temperature], true, true, true);
+		chartTH.series[sensorId - 1].addPoint([x, temperature], true, true, true); // redraw, shift, animation, with event
 	} else {
 		chartTH.series[sensorId - 1].addPoint([x, temperature], true, false, true);
 	}
@@ -103,7 +103,39 @@ socket.on("experimentList", (res) => {
 
 socket.on("accData", (accNum, xyz, accelerometerTimingMillis, value) => {
 	console.log(`${accNum} ${xyz} ${accelerometerTimingMillis} ${value}`);
+	const accel = parseFloat(value); 
+	
+	let accelAxis;
+	switch(xyz) {
+		case "x":
+		accelAxis = (accNum - 1) * 3; // index 0 and 3
+		break;
+		case "y":
+		accelAxis = (accNum - 1) * 3 + 1; // index 1 and 4
+		break;
+		case "z":
+		accelAxis = (accNum - 1) * 3 + 2; // index 2 and 5
+		break;
+		default:
+		// how to error
+		console.log('"Incorrect Axis given"');
+		break;
+	}
+		
+	if (chartA.series[accelAxis].data.length > 100) {
+		chartA.series[accelAxis].addPoint([accelerometerTimingMillis, accel], true, true, true);
+	} else {
+		chartA.series[accelAxis].addPoint([accelerometerTimingMillis, accel], true, false, true);
+	}
 });
+
+function resetAccel () { // call this function for when the RPM changes
+	chartA.series.forEach(series => { 
+		series.setData([], false);
+	});
+	chartA.reDraw();
+}
+
 
 socket.emit("experimentdataRequest");
 
