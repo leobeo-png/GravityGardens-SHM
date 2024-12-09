061c4944ecdb4bb589021784fd23aa859b9e23d9
SemWissink <169163398+SemWissink@users.noreply.github.com>
2024-12-09 10:02:19 +0100

    required librarys

---
+++ Firmware/main_SDM/CHT.h
@@ -0,0 +1,15 @@
+#ifndef CHT8305_H
+#define CHT8305_H
+
+#include <Arduino.h>  // Include Arduino.h for String and other definitions
+#include "CHT8305.h"
+
+// External declarations for CHT8305 sensor objects
+extern CHT8305 cht1;
+extern CHT8305 cht2; 
+
+// Function declarations
+String readCHTTemperature(CHT8305& cht);
+String readCHTHumidity(CHT8305& cht);
+
+#endif // CHT8305_H
